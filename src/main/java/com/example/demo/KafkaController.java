386d111d9fa7ba0d9df01da0ab88c45f0c9eacc9
aditya <aditya@gmail.com>
2020-04-29 10:40:08 +0530

    first commit

---
+++ src/main/java/com/example/demo/KafkaController.java
@@ -0,0 +1,59 @@
+package com.example.demo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+@RequestMapping(value = "/kafka")
+
+public class KafkaController {
+
+private final Producer producer;
+
+@Autowired
+
+public KafkaController(Producer producer) {
+
+this.producer = producer;
+
+}
+
+@PostMapping(value = "/receive")
+
+public void sendMessageToKafkaTopic(@RequestParam("message") String message) throws IOException{
+
+this.producer.sendMessage(message);
+receivefromHttp();
+
+}
+public void receivefromHttp() throws IOException {
+//	String command =
+//			  "curl -X GET http://localhost:30896/metrics";
+//	Process process = Runtime.getRuntime().exec(command);
+//	System.out.println(command);
+//	System.out.println(process.getInputStream().toString());
+	 URL yahoo = new URL("http://localhost:30985/metrics");
+     URLConnection yc = yahoo.openConnection();
+     BufferedReader in = new BufferedReader(
+                             new InputStreamReader(
+                             yc.getInputStream()));
+     String inputLine;
+     
+     while ((inputLine = in.readLine()) != null) 
+         //System.out.println(inputLine);
+     this.producer.sendMessage(inputLine);
+     in.close();
+	
+}
+
+}
