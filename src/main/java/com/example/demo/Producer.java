d151a8183290d880d60f402537adc8a8cc851385
aditya <aditya@gmail.com>
2020-04-29 11:13:18 +0530

    first commitdddf

--- src/main/java/com/example/demo/Producer.java
+++ src/main/java/com/example/demo/Producer.java
@@ -10,13 +10,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
-
+@Configuration
 public class Producer {
 
 private static final Logger logger = LoggerFactory.getLogger(Producer.class);
